bead5d9524fd5cd41a5e35d2486c00248f20ee27
dpkpathak12 <deepak.pahak7@gmail.com>
2022-02-03 17:09:26 +0530

    adding the scheduler with lambda

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "aws_instance" "nginx_server" {
       "sudo apt install nginx -y",
       "sudo service ngnix start",
       "echo Welcome to Nginx-Server - Virtual Server is at ${self.public_dns} | sudo tee /var/www/html/index.html",
-      "(crontab -l 2>/dev/null; echo " 0 1 * * * init 0 ") | crontab -"
+      //"(crontab -l 2>/dev/null; echo " 0 1 * * * init 0 ") | crontab -"
       
     ]
      
